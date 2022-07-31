f6617a2b5cf0f9149b0c5ddc2cd34e0e1fae5ef4
Marina Chialvo <chialvomar@gmail.com>
2022-07-31 17:25:44 -0300

    proyecto final

--- js/main.js
+++ js/main.js
@@ -2,23 +2,20 @@
 import { obtenerDatos } from './turnos.js'
 
 const domTurnos = document.getElementById('mainTurnos')
+const domCarrito = document.getElementById('carrito')
+const fCompra = document.getElementById('finCompra')
 
-//evento
+let carrito = []
+
+
+//evento para cuando se carga la pagina, ejecute la funcion
 window.addEventListener('DOMContentLoaded', () =>{
 	renderizarDOM()
 })
 
-const mostrarDatos = async() => {
- 	let datos = await obtenerDatos()
-  	console.log(datos)
-}
-
-mostrarDatos()
-
-
 const renderizarDOM = async() =>{
- 	let productos = await obtenerDatos()
 	let productosVista = ''
+	let productos = await obtenerDatos()
  	productos.forEach(producto =>{
 		const {img, nombre, demora, precio, desc, id} = producto
 		{
@@ -35,7 +32,7 @@ const renderizarDOM = async() =>{
 				  	<p id=precioProducto>$${precio}</p> 
 				  	<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 					<p id=descProducto class="card-text2">${desc}</p>
-				  	<button data-id"${id}" id="mybtn" name="btnSeleccionar" class="btn btn-primary">Seleccionar</button>
+				  	<button data-id="${id}" id="mybtn" name="btnSeleccionar" class="btn btn-primary">Seleccionar</button>
 					</div>
 			    </div>
 				</div>
@@ -47,24 +44,109 @@ const renderizarDOM = async() =>{
 }
 
 //evento para el boton Seleccionar y que se guarde en el LS
-domTurnos.addEventListener("click", (e) => {
+domTurnos.addEventListener('click', (e) => {
 	if (e.target.id === "mybtn"){
-		guardarTurnos(e.target.dataset.id) //recupero la data del elemento que se quiere seleccionar
+		Swal.fire({
+			title: '¡Perfecto! Agregaste un item.',
+			icon: 'success',
+			showClass: {
+			  popup: 'animate__animated animate__fadeInDown'
+			},
+			hideClass: {
+			  popup: 'animate__animated animate__fadeOutUp'
+			}
+		  })
+		guardarTurnos(e.target.dataset.id); //recupero la data del elemento que se quiere seleccionar
 	}
 })
 
 //funcion que busca el elemento con ese id del stock
-	const guardarTurnos = async (id) => {
+ 	const guardarTurnos = async(id) => {
 	let productos = await obtenerDatos()
 	let productoEncontrado = productos.find(producto => producto.id === parseInt(id));
 	let productoStorage = JSON.parse(localStorage.getItem(id));
 	if(productoStorage === null){
-		localStorage.setItem(id, JSON.stringify({...productoEncontrado, Cantidad:1})) //devuelve todas las prop de un objeto o array.
+		localStorage.setItem(id, JSON.stringify({...productoEncontrado, cantidad:1})) //devuelve todas las prop de un objeto o array.
+		recorrerStorage() //con esta funcion actualizo el carrito.
 	}else{
 		let productoExiste = JSON.parse(localStorage.getItem(id))
+		productoExiste.cantidad = productoExiste.cantidad + 1
 		productoExiste.precio = productoExiste.precio + productoEncontrado.precio
-		productoExiste.Cantidad = productoExiste.Cantidad + 1
-		console.log(productoExiste)
 		localStorage.setItem(id, JSON.stringify(productoExiste))
+		recorrerStorage()
 	}
 }
+
+//contenedor donde guardamos los items
+const recorrerStorage = () => {
+    carrito.length = 0 //ponemos el carrito en 0 para que no se pisen los productos
+    for (let i = 0; i < localStorage.length; i++) {
+        let clave = localStorage.key(i)
+        carrito.push(JSON.parse(localStorage.getItem(clave)))
+    }
+	renderCarrito()
+}
+
+const renderCarrito = () => {
+	if (carrito.length > 0){
+		domCarrito.innerHTML = ''
+		carrito.forEach(producto => {
+			const {img, nombre, demora, precio, desc, id, cantidad} = producto
+			domCarrito.innerHTML += `
+			<div id="carrito" class="card" style="width: 18rem;">
+				<img id=fotoProducto src="${img}" class="card-img-top" alt="...">
+				<div class="card-body">
+			  <p id="descProducto" class="card-text2">Seleccionaste ${nombre}</p>
+			  <p id=precioProducto>$${precio}</p>
+			  <p>Cantidad: ${cantidad}</p> 
+			  <button id="eliminar" data-id="${id}" type="button" class="btn btn-secondary">Eliminar</button>
+				</div>
+				</div>`			
+		});
+	} else{
+		domCarrito.innerHTML = `
+		<div id="carritoVacio" class="card mb-3" style="max-width: 540px;">
+		<div class="col-md-4 col-sm-4">
+  			<img src="../img/carritovacio.png" class="card-img-top" alt="bolsa de compras vacia.">
+		<div class="card-body">
+		</div>
+			<p class="card-text">No hay nada seleccionado.</p>
+		</div>
+		</div>`
+	}
+}
+
+domCarrito.addEventListener('click', borrarProducto)
+
+function borrarProducto(e) {
+	if (e.target.id === "eliminar"){
+		let idProducto = e.target.dataset.id
+		//let productoEncontrado = productos.find(producto => producto.id === parseInt(idProducto))
+		let productoStorage = JSON.parse(localStorage.getItem(idProducto))
+		if(productoStorage.cantidad > 1){
+			productoStorage.cantidad = productoStorage.cantidad - 1
+			productoStorage.precio = productoStorage.precio - productoEncontrado.precio
+			localStorage.setItem(idProducto, JSON.stringify(productoStorage))
+			recorrerStorage()
+		}else{
+			localStorage.removeItem(idProducto)
+			recorrerStorage()
+		}
+	}
+}
+recorrerStorage()
+
+fCompra.addEventListener('click',(e) => {
+	Swal.fire({
+		title: 'Finalizaste tu compra :)',
+		icon: 'success',
+		showClass: {
+		  popup: 'animate__animated animate__fadeInDown'
+		},
+		hideClass: {
+		  popup: 'animate__animated animate__fadeOutUp'
+		}
+	  })	
+})
+
+
